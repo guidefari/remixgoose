788027a01ce128826217fc32420a7189eefe3420
Guide Fari <guideg6@gmail.com>
2022-07-02 22:46:40 +0200

    more basic routing to check out deploy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,14 @@
+import { Link } from "@remix-run/react"
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul className="list-none">
+    <>
+      <h1>Artists</h1>
+      <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-        <li className="">
-          <h1 className="text-3xl font-bold underline">Got tailwind now mf</h1>
+          <Link to="/artists/barcode">Barcode</Link>
         </li>
       </ul>
-    </div>
+    </>
   )
 }
