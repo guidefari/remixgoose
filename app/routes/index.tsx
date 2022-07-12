7ed341bdd030a7b76d7a7fc964fa5519b34f600f
Guide <guideg6@gmail.com>
2022-07-12 07:19:03 +0200

    feat: add tailwind typography
    sane defaults to start with

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,13 +2,13 @@ import { Link } from "@remix-run/react"
 
 export default function Index() {
   return (
-    <>
+    <article className="prose-xl">
       <h1>Artists</h1>
       <ul>
         <li>
           <Link to="/artists/barcode">Barcode</Link>
         </li>
       </ul>
-    </>
+    </article>
   )
 }
